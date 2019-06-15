f2797fa5127dcffefeff84ff9e41caaf27eede5f
JohhanYu <1115044813@qq.com>
2019-06-15 10:28:08 +0800

    创建注册中心

---
+++ EurekaServer/src/main/java/com/SpringCloud/EurekaServer/EurekaServer.java
@@ -0,0 +1,13 @@
+package com.SpringCloud.EurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringCloudApplication
+@EnableEurekaServer
+public class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class,args);
+    }
+}
